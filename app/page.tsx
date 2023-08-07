3230b868ca6c57b384a2bb13da7b62236843acbd
Ari Ross <ari.ross@ef.com>
2023-08-07 08:43:16 -0400

    init

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+/** Add your relevant code here for the issue to reproduce */
+export default function Home() {
+  return (
+    <>
+      <input type="radio" name="example" id="option1" value="1" />
+      <input type="radio" name="example" id="option2" value="2" />
+      <input type="radio" name="example" id="option3" value="3" />
+    </>
+  );
+}
